9d9c170fdc6892b8106020c047a2286dbe5d7bac
Nathan Petrangelo <nathan@petrangelo.org>
2023-09-07 17:46:06 -0400

    Rendering LEDStrip, not connected to model

--- src/ledStrip.rs
+++ src/ledStrip.rs
@@ -1,4 +1,29 @@
+use nannou::prelude::*;
+
 #[derive(Debug)]
-struct LEDs {
+pub struct LEDStrip {
     leds: Vec<(i32, i32, i32)>,
+}
+
+impl LEDStrip {
+    pub fn new(numLeds: usize) -> Self {
+        Self {
+            leds: vec!((0,0,0); numLeds),
+        }
+    }
+    pub fn draw(&self, draw: &Draw) {
+        let r = 10;
+        let h = r * 2 + 10;
+        let w = h * self.leds.len() as i32;
+        draw.rect()
+            .w_h((w + h) as f32,h as f32)
+            .color(GRAY);
+
+        for i in (-w/2..=w/2).step_by(h as usize) {
+            draw.ellipse()
+                .color(RED)
+                .radius(r as f32)
+                .x_y(i as f32, 0.0);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod ledStrip;
-
+use ledStrip::LEDStrip;
 use nannou::prelude::*;
 
 fn main() {
@@ -10,34 +10,19 @@ fn main() {
 }
 
 struct Model {
-    leds: Vec<(i32, i32, i32)>,
+    leds: LEDStrip,
 }
 
 fn model(_app: &App) -> Model {
-    Model { leds: Vec::new() }
+    Model { leds: LEDStrip::new(20) }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    // frame.clear(PURPLE);
     let draw = app.draw();
     draw.background().color(PLUM);
-    // Draw a blue ellipse with default size and position.
-    draw.ellipse()
-        .color(STEELBLUE)
-        .w(300.0)
-        .h(200.0)
-        .x_y(200.0, -100.0);
-
-    let w = app.window_rect().w() as i32;
-    for i in (-w/2..w/2).step_by(30) {
-        draw.ellipse()
-            .color(RED)
-            .radius(10.0)
-            .x_y(i as f32, -100.0);
-    }
-
+    model.leds.draw(&draw);
     draw.to_frame(app, &frame).unwrap();
 }
