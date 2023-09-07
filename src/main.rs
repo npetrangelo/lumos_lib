8281e14494164dbb7fc21ae0d8d9d4eaeb3156f7
Nathan Petrangelo <nathan@petrangelo.org>
2023-09-07 15:36:01 -0400

    Trying to draw row of circles, can't iterate over window x range for some reason

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod LEDs;
+mod ledStrip;
 
 use nannou::prelude::*;
 
@@ -9,15 +9,33 @@ fn main() {
         .run();
 }
 
-struct Model {}
+struct Model {
+    leds: Vec<(i32, i32, i32)>,
+}
 
 fn model(_app: &App) -> Model {
-    Model {}
+    Model { leds: Vec::new() }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame){
-    frame.clear(PURPLE);
+fn view(app: &App, model: &Model, frame: Frame) {
+    // frame.clear(PURPLE);
+    let draw = app.draw();
+    draw.background().color(PLUM);
+    // Draw a blue ellipse with default size and position.
+    draw.ellipse()
+        .color(STEELBLUE)
+        .w(300.0)
+        .h(200.0)
+        .x_y(200.0, -100.0);
+
+    for i in app.window_rect().x.step_by(50) {
+        draw.ellipse()
+            .color(RED)
+            .x_y(i as f32, -100.0);
+    }
+
+    draw.to_frame(app, &frame).unwrap();
 }
